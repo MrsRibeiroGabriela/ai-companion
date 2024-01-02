7ad150b2e63ba53d8f1d289fe5c687faf0f8be7b
Gabriela Ribeiro <mrsribeirogabriela@MacBook-Pro-de-Gabriela.local>
2024-01-02 15:33:19 -0300

    Ui: Chat message page body

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,8 @@
 import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+
 import { CompanionForm } from "./components/companion-form";
+
 
 interface CompanionIdPageProps {
   params: {
@@ -9,11 +12,17 @@ interface CompanionIdPageProps {
 
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
   //TODO: Check subscription
+  const { userId } = auth();
+
+  if(!userId) {
+    return redirectToSignIn();
+  }
 
   //server component
   const companion = await prismadb.companion.findUnique({
     where: {
       id: params.companionId,
+      userId
     },
   });
 
