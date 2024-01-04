e1bde17bef4d82daf1fe6d1bb3fae0bb8acc5d52
Gabriela Ribeiro <mrsribeirogabriela@MacBook-Pro-de-Gabriela.local>
2024-01-04 04:00:16 -0300

    Fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = ({ isPro }: NavbarProps) => {
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
         {/* <Menu className="block md:hidden" /> */}
-        <MobileSidebar />
+        <MobileSidebar isPro={isPro}/>
         <Link href="/">
           <h1
             className={cn(
